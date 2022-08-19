401f38c875992409c7c74f27ef6b55ec3d18ff16
jimlin2004 <jim.lin2004@gmail.com>
2022-08-19 20:41:10 +0800

    新增輪替目錄，修復border的邊線問題

--- index/js/DisplayMenu.js
+++ index/js/DisplayMenu.js
@@ -8,6 +8,7 @@ class DisplayMenu
     {
         this.data = null;
         this.swiper = new SwiperSystem();
+        this.currentImgIndexStart = 0;
     }
 
     async loadJson()
@@ -29,58 +30,131 @@ class DisplayMenu
         }
     }
 
-    addToHTML()
+    checkImgIndex(index)
     {
-        let labels_div = document.getElementById("labels");
+        if (index >= this.data["labels"].length)
+            return index - this.data["labels"].length;
+        else if (index < 0)
+            return this.data["labels"].length - 1;
+        return index;
+    }
+
+    updateLabel()
+    {
+        let label_items = document.getElementById("labels").querySelectorAll(".labels_item");
+        let currentImgIndex = 0;
+        let labelData = null;
         let labels_item_div = null;
-        let label_pushpin = null;
         let label_title_p = null;
+        let label_pushpin = null;
         let label_content_div = null;
         let label_img = null;
-        for (let item of this.data["labels"])
+        for (let i = 0; i < 3; i++)
         {
-            labels_item_div = document.createElement("div");
-            labels_item_div.className = "labels_item";
-            labels_item_div.id = item["id"];
+            currentImgIndex = this.checkImgIndex(this.currentImgIndexStart + i);
+            // console.log(currentImgIndex);
+            labelData = this.data["labels"][currentImgIndex];
+            labels_item_div = label_items[i];
+            labels_item_div.id = labelData["id"];
+            label_title_p = labels_item_div.querySelector(".label_title");
+            label_title_p.innerHTML = labelData["title"]["title"];
+            label_title_p.style.backgroundColor = labelData["title"]["color"];
             label_pushpin = document.createElement("img");
             label_pushpin.src = "./img/pushpin.png";
             label_pushpin.className = "pushpin";
-            label_title_p = document.createElement("p");
-            label_title_p.id = "label_title";
-            label_title_p.innerHTML = item["title"]["title"];
-            label_title_p.style.backgroundColor = item["title"]["color"];
-            label_content_div = document.createElement("div");
-            label_content_div.id = "label_content";
-            // label_content_div.innerHTML = item["description"];
-            this.splitTextToP(label_content_div, item["description"]);
-            label_img = document.createElement("img");
-            label_img.id = "label_img";
-            label_img.src = item["img"];
-
             label_title_p.appendChild(label_pushpin);
-            labels_item_div.appendChild(label_title_p);
-            labels_item_div.appendChild(label_content_div);
-            labels_div.appendChild(labels_item_div);
+            label_content_div = labels_item_div.querySelector(".label_content");
+            label_content_div.innerHTML = "";
+            this.splitTextToP(label_content_div, labelData["description"]);
+            label_img = labels_item_div.querySelector(".label_img");
+            label_img.src = labelData["img"];
             //<改變照片大小>
-            label_img.width = label_content_div.offsetWidth;
-            // label_img.height = label_content_div.offsetHeight;            
-            labels_item_div.appendChild(label_img);
+            label_img.width = label_content_div.offsetWidth;  
             //</改變照片大小>
-            this.swiper.pushNewLabel(labels_item_div, item["title"]["title"], "<div>hi</div>");
+            // label_title_p.width = label_content_div.offsetWidth;
+            this.swiper.pushNewLabel(labels_item_div, labelData["title"]["title"], "<div>hi</div>");
         }
+        return;
     }
+
+    ToPreviousLabel()
+    {
+        this.currentImgIndexStart--;
+        if (this.currentImgIndexStart < 0)
+        {
+            this.currentImgIndexStart = this.data["labels"].length - 1;
+        }
+        this.updateLabel();
+        return;
+    }
+
+    ToNextLabel()
+    {
+        this.currentImgIndexStart++;
+        if (this.currentImgIndexStart >= this.data["labels"].length)
+        {
+            this.currentImgIndexStart = 0;
+        }
+        this.updateLabel();
+        return;
+    }
+
+    // addToHTML()
+    // {
+    //     let labels_div = document.getElementById("labels");
+    //     let labels_item_div = null;
+    //     let label_pushpin = null;
+    //     let label_title_p = null;
+    //     let label_content_div = null;
+    //     let label_img = null;
+    //     for (let item of this.data["labels"])
+    //     {
+    //         labels_item_div = document.createElement("div");
+    //         labels_item_div.className = "labels_item";
+    //         labels_item_div.id = item["id"];
+    //         label_pushpin = document.createElement("img");
+    //         label_pushpin.src = "./img/pushpin.png";
+    //         label_pushpin.className = "pushpin";
+    //         label_title_p = document.createElement("p");
+    //         label_title_p.id = "label_title";
+    //         label_title_p.innerHTML = item["title"]["title"];
+    //         label_title_p.style.backgroundColor = item["title"]["color"];
+    //         label_content_div = document.createElement("div");
+    //         label_content_div.id = "label_content";
+    //         // label_content_div.innerHTML = item["description"];
+    //         this.splitTextToP(label_content_div, item["description"]);
+    //         label_img = document.createElement("img");
+    //         label_img.id = "label_img";
+    //         label_img.src = item["img"];
+
+    //         label_title_p.appendChild(label_pushpin);
+    //         labels_item_div.appendChild(label_title_p);
+    //         labels_item_div.appendChild(label_content_div);
+    //         labels_div.appendChild(labels_item_div);
+    //         //<改變照片大小>
+    //         label_img.width = label_content_div.offsetWidth;
+    //         // label_img.height = label_content_div.offsetHeight;            
+    //         labels_item_div.appendChild(label_img);
+    //         //</改變照片大小>
+    //         this.swiper.pushNewLabel(labels_item_div, item["title"]["title"], "<div>hi</div>");
+    //     }
+    // }
 };
 
 $(function() {
     let displayMenu = new DisplayMenu();
+    let bnt_previous = document.getElementById("bnt_previous");
+    let bnt_next = document.getElementById("bnt_next");
+    bnt_previous.addEventListener("click", () => {
+        displayMenu.ToPreviousLabel();
+    })
+    bnt_next.addEventListener("click", () => {
+        displayMenu.ToNextLabel();
+    })
     displayMenu.loadJson()
         .then(() => {
-            displayMenu.addToHTML();
+            // displayMenu.addToHTML();
+            displayMenu.updateLabel();
             displayMenu.swiper.handleEvent(document.getElementById("labels"));
-            // console.log("hi");
         });
-    // let tmp = document.createElement("button");
-    // tmp.innerHTML = "Hi";
-    // document.getElementById("swiper_container").appendChild(tmp);
-    // console.log(data);
 });
