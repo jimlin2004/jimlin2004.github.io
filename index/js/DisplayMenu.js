0df9bc09f747b3b24854a48e1dde5f90c506cfc9
jimlin2004 <jim.lin2004@gmail.com>
2022-08-31 20:35:31 +0800

    修復css 2022/8/31

--- index/js/DisplayMenu.js
+++ index/js/DisplayMenu.js
@@ -47,13 +47,11 @@ class DisplayMenu
         let labels_item_div = null;
         let label_title_p = null;
         let label_pushpin = null;
-        // let label_content_div = null;
         let label_content_text_div = null;
         let label_img = null;
         for (let i = 0; i < 3; i++)
         {
             currentImgIndex = this.checkImgIndex(this.currentImgIndexStart + i);
-            // console.log(currentImgIndex);
             labelData = this.data["labels"][currentImgIndex];
             labels_item_div = label_items[i];
             labels_item_div.id = labelData["id"];
@@ -69,10 +67,6 @@ class DisplayMenu
             this.splitTextToP(label_content_text_div, labelData["description"]);
             label_img = labels_item_div.querySelector(".label_img");
             label_img.src = labelData["img"];
-            //<改變照片大小>
-            // label_img.width = label_content_div.offsetWidth;  
-            //</改變照片大小>
-            // label_title_p.width = label_content_div.offsetWidth;
             this.swiper.pushNewLabel(labels_item_div, labelData["title"]["title"], labelData["complete_description"]);
         }
         return;
@@ -99,47 +93,6 @@ class DisplayMenu
         this.updateLabel();
         return;
     }
-
-    // addToHTML()
-    // {
-    //     let labels_div = document.getElementById("labels");
-    //     let labels_item_div = null;
-    //     let label_pushpin = null;
-    //     let label_title_p = null;
-    //     let label_content_div = null;
-    //     let label_img = null;
-    //     for (let item of this.data["labels"])
-    //     {
-    //         labels_item_div = document.createElement("div");
-    //         labels_item_div.className = "labels_item";
-    //         labels_item_div.id = item["id"];
-    //         label_pushpin = document.createElement("img");
-    //         label_pushpin.src = "./img/pushpin.png";
-    //         label_pushpin.className = "pushpin";
-    //         label_title_p = document.createElement("p");
-    //         label_title_p.id = "label_title";
-    //         label_title_p.innerHTML = item["title"]["title"];
-    //         label_title_p.style.backgroundColor = item["title"]["color"];
-    //         label_content_div = document.createElement("div");
-    //         label_content_div.id = "label_content";
-    //         // label_content_div.innerHTML = item["description"];
-    //         this.splitTextToP(label_content_div, item["description"]);
-    //         label_img = document.createElement("img");
-    //         label_img.id = "label_img";
-    //         label_img.src = item["img"];
-
-    //         label_title_p.appendChild(label_pushpin);
-    //         labels_item_div.appendChild(label_title_p);
-    //         labels_item_div.appendChild(label_content_div);
-    //         labels_div.appendChild(labels_item_div);
-    //         //<改變照片大小>
-    //         label_img.width = label_content_div.offsetWidth;
-    //         // label_img.height = label_content_div.offsetHeight;            
-    //         labels_item_div.appendChild(label_img);
-    //         //</改變照片大小>
-    //         this.swiper.pushNewLabel(labels_item_div, item["title"]["title"], "<div>hi</div>");
-    //     }
-    // }
 };
 
 $(function() {
@@ -154,7 +107,6 @@ $(function() {
     })
     displayMenu.loadJson()
         .then(() => {
-            // displayMenu.addToHTML();
             displayMenu.updateLabel();
             displayMenu.swiper.handleEvent(document.getElementById("labels"));
         });
