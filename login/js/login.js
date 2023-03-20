d94822b94247be99686c6bc20b82cce00d043e86
jimlin2004 <jim.lin2004@gmail.com>
2023-03-20 22:17:07 +0800

    2023/03/20 --verion 1.0dev

---
+++ login/js/login.js
@@ -0,0 +1,26 @@
+//解析login form的資料
+function parseFormData() 
+{
+    let formData = new FormData(document.getElementById("login_form"));
+    return formData;
+}
+
+//向後端傳送資料並接收資料
+function uploadData(formData)
+{
+    fetch("http://localhost:8000/api/login.php", {
+        method: "POST",
+        body: formData,
+        headers: {"Content-Type": "multipart/form-data"}
+    })
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json);
+    })
+}
+
+const loginForm = document.getElementById("login_form");
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    uploadData(parseFormData());
+});
