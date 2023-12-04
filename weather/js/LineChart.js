d284913d4730152c0d06b38df30351f0071953af
Jimlin2004 <74666312+jimlin2004@users.noreply.github.com>
2023-12-04 14:00:54 +0800

    Update LineChart.js

--- weather/js/LineChart.js
+++ weather/js/LineChart.js
@@ -110,6 +110,7 @@ class LineChart extends Chart
                         //到06:00的是晚上，到18:00的是早上
                         if (d.getHours() === 6)
                         {
+                            console.log(d.getDate() - 1);
                             d.setDate(d.getDate() - 1);
                             return d3.timeFormat("晚上<br/>%m/%d")(d);
                         }
